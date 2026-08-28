fe13b6eafea7cf76f287ae8b1eb485cc96519ecb
Alex-M-2013 <alexmach01@outlook.com>
2026-08-28 14:39:59 +0100

    Update search function

--- search.js
+++ search.js
@@ -6,8 +6,9 @@ function search() {
     monsterCards.forEach((card) => {
         const monsterName = removeDashes(card.querySelector("p").textContent).replace("Name: ", "").toLowerCase();
         const monsterType = removeDashes(card.querySelector(".monster-type").textContent).replace("Type: ", "").toLowerCase();
+        const monsterSpecies = removeDashes(card.querySelector(".monster-species").textContent).replace("Species: ", "").toLowerCase();
 
-        if (monsterName.includes(userSearch) || monsterType.includes(userSearch)) {
+        if (monsterName.includes(userSearch) || monsterType.includes(userSearch) || monsterSpecies.includes(userSearch)) {
             card.style.display = "";
         } else {
             card.style.display = "none";
